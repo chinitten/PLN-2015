47380ee380697cbdc3322618c23de26808a0ed75
Hernán Nieto <chinitten@gmail.com>
2015-09-22 19:19:04 -0300

    Agregado generate.py

---
+++ languagemodeling/scripts/generate.py
@@ -0,0 +1,32 @@
+""" Generate natural language sentences using a language model.
+
+Usage:
+  generate.py -i <file> -n <n>
+  generate.py -h | --help
+
+Options:
+  -i <file>     Language model file.
+  -n <n>        Number of sentences to generate.
+  -h --help     Show this screen.
+"""
+
+from docopt import docopt
+import pickle
+
+from languagemodeling.ngram import NGramGenerator
+
+if __name__ == '__main__':
+    opts = docopt(__doc__)
+
+    filename = opts['-i']
+    f = open(filename, 'rb')
+    model = pickle.load(f)
+
+
+    n = int(opts['-n'])
+
+    generator = NGramGenerator(model)
+    sents = list()
+    for i in range(0,n):
+        sents.append(generator.generate_sent())
+    print(sents)
