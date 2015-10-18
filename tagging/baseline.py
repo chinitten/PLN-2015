6c7fb3c82476dc7de30f228d610e14957a37e95d
Hernán Nieto <chinitten@gmail.com>
2015-10-17 23:41:43 -0300

    Completo baseline.py

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict
 
 class BaselineTagger:
 
@@ -6,7 +6,14 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+        self.tagworddict = tagworddict = defaultdict(lambda: defaultdict(int))
+        tagcountdict = defaultdict(int)
+        for sent in tagged_sents:
+            for word,tag in sent:
+                tagworddict[word][tag] += 1
+                tagcountdict[tag] += 1
+        self.most_freq_tag = max(tagcountdict.items(), key=lambda x: x[1])[0]
+        self.tagworddict = dict(tagworddict)
 
     def tag(self, sent):
         """Tag a sentence.
@@ -20,11 +27,16 @@ class BaselineTagger:
 
         w -- the word.
         """
-        return 'nc'
+        if self.unknown(w):
+            return self.most_freq_tag
+        return max(self.tagworddict[w].items(), key = lambda x: x[1])[0]
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
         w -- the word.
         """
+        value = self.tagworddict.get(w,True)
+        if value!=True:
+            return False
         return True
